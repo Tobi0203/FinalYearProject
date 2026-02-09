336292257d708bab8abb70cc8a87588b6b8d5d53
Tobi0203 <sk.roshansameers.h.k@gmail.com>
2026-02-09 20:15:28 +0530

    finishing touch

--- frontend/src/Utils/Socket.js
+++ frontend/src/Utils/Socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket=io("http://localhost:5000",{
+const socket=io("https://social-media-backend-bd38.onrender.com",{
     withCredentials:true,
 });
 export default socket;
