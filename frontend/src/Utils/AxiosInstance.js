a7c42e91e2e114b3b549d356700eca3d29ffc9ae
Tobi0203 <sk.roshansameers.h.k@gmail.com>
2026-02-09 20:21:15 +0530

    finishing touch

--- frontend/src/Utils/AxiosInstance.js
+++ frontend/src/Utils/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosIns=axios.create({
-    baseURL:"https://final-year-project-ten-mauve.vercel.app/",
+    baseURL:"https://social-media-backend-bd38.onrender.com",
     // headers:{
     //     "Content-Type":"application/json"
     // },
